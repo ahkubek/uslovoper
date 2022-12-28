59695dbce56471a2b26285bdf4b1a83860e962c2
ahkubek <120653318+ahkubek@users.noreply.github.com>
2022-12-28 18:04:06 +0300

    Домашка УСЛОВНЫЕ ОПЕРАТОРЫ-задача2

--- src/Main.java
+++ src/Main.java
@@ -2,12 +2,18 @@ public class Main {
     public static void main(String[] args) {
 
 int age = 18;
+int temperatura = 30;
 if (age>=18){
             System.out.println("Ты совершеннолетний и тебе можно все");
 
         } else {
     System.out.println("Возраст совершеннолетия еще не наступил и нужно немного подождать.");
         }
+if (temperatura<=5){
+    System.out.println("холодно");
+            } else {
+    System.out.println ("температура нормальная");
+            }
 
 
     }
